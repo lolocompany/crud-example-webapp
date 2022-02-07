8ff0490b6dca63efd3470323ef9ea3ef60c26f6e
Anders Wittfeldt <anders@lolo.company>
2022-02-07 14:11:07 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { LoloAdmin, LoloResource } from '@lolocompany/react-admin-lolo';
+
+function App() {
+  return (
+    <LoloAdmin
+      apiUrl={process.env.REACT_APP_API_URL}
+      title='CRUD example'
+    >
+      <LoloResource name='notes' />
+      <LoloResource name='boards' />
+      <LoloResource name='users' />
+    </LoloAdmin>
+  );
+}
+
+export default App;
